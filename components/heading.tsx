0ea8dd6f64173153c9edd4a801f0fcbcf62ce4a1
ciobiano <rafaelobiano@yahoo.com>
2024-04-16 00:22:59 +0100

    feat: Add a Heading, button, alert  component

---
+++ components/heading.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+interface HeadingProps {
+	title: string;
+	spanText?: string;
+	subtitle?: string;
+	description?: string;
+	center?: boolean;
+}
+
+const Heading: React.FC<HeadingProps> = ({
+	title,
+	subtitle,
+	center,
+	description,
+	spanText,
+}) => {
+	return (
+		<div className={center ? "text-center" : "text-start"}>
+			<div className="flex items-center mb-4">
+				<div className="flex w-20 mr-4">
+					<div className="w-1/3 border-red-500 border-t-2" />
+					<div className="w-3/4 border-gray-300 border-t-2" />
+				</div>
+				<h1 className="text-sm ">{subtitle}</h1>
+			</div>
+			<div className="text-5xl leading-tight text-primary font-normal max-w-[600px]">
+				{title} <br />
+				<span className="text-5xl bg-gradient-to-r from-[#D51D12] to-[#EF4A32]  text-transparent bg-clip-text font-medium">
+					{spanText}
+				</span>
+			</div>
+			<div className="font-medium text-neutral-500 text-xl mt-2">
+				{description}
+			</div>
+		</div>
+	);
+};
+
+export default Heading;
