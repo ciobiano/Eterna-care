366313ab8732787e446777e9a286dacebcd72f97
ciobiano <rafaelobiano@yahoo.com>
2024-06-25 14:59:04 +0100

    refactor: Remove unused searchBox component from NavBar

--- components/NavBar/nav.tsx
+++ components/NavBar/nav.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 import Logo from "../Logo";
-import SearchBox from "./searchBox";
 import MainNav from "./mainNav";
 
 const NavBar = () => {
--- components/NavBar/searchBox.tsx
+++ components/NavBar/searchBox.tsx
@@ -1,5 +1,4 @@
 import { Search } from "lucide-react";
-import React from "react";
 
 const SearchBox = () => {
 	return (
